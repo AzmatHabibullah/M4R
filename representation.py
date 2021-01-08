eeab74e93d6771d9815737b0ac77924ed1aa6613
Azmat Habibullah <ah5117@ic.ac.uk>
2021-01-08 13:43:03 +0000

    add draw as point and cartesian projection

--- representation.py
+++ representation.py
@@ -71,16 +71,68 @@ def draw_spinor_projection(spinor, gauge=1, d3=True, clear=True, return_states=F
     if return_states:
         return states
 
-# todo implement a function which draws states as points
 # todo implement a function which draws lines (use point parameter method='l')
 # todo draw animation of moving spinors around a curve (eg phi = 0)
 # todo draw animation of moving spinors around arbitrary curve
 # todo calculate Berry phase
-# todo compute inversion formula
 
-def refresh():
-    global N
-    N = Qobj(np.array([np.sin(eta) / np.sqrt(2), 0, np.cos(eta), 0, np.sin(eta) / np.sqrt(2)]))
+
+def construct_polar_states(states, gauge=1):
+    n = len(states)
+    F = n/2
+    thetas = np.zeros(n, dtype=complex)
+    phis = np.zeros(n, dtype=complex)
+    for k in range(n):
+        if gauge ==1:
+            thetas[k] = 2*np.arccos(states[k][0][0])
+            if thetas[k] != 0:
+                phis[k] = np.log(states[k][1][0]/np.sin(thetas[k]/2))/1.0j # todo fix branch cut issues
+            else:
+                phis[k] = 0
+    thetas = thetas.real
+    phis = phis.real
+    return thetas, phis
+
+
+def reconstruct_spinor(thetas, phis):
+    n = thetas.size
+    F = n/2
+    roots = [thetas[i] * np.e ** (1.0j * phis[i]) for i in range(n)]
+    poly_coeffs = np.polynomial.polynomial.polyfromroots(roots) # this is monic! see rescaling issues
+    spinor_coeffs = np.zeros(int(2*F + 1), dtype=complex)
+    for k in range(int(2*F + 1)):
+        spinor_coeffs[k] = poly_coeffs[k]/np.sqrt(comb(int(2*F), k)).conj()
+    return Qobj(spinor_coeffs)
+
+
+def polar_to_point(thetas, phis):
+    n = thetas.size
+    x = np.sin(thetas) * np.cos(phis)
+    y = np.sin(thetas) * np.sin(phis)
+    z = np.cos(thetas)
+    return x, y, z
+
+
+def draw_points(spinor, thetas=None, phis=None, d3=True, clear=True, method='s', return_points=False):
+    if thetas is None or phis is None:
+        _, thetas, phis = projection_onto_spin_half(spinor)
+    x, y, z = polar_to_point(thetas, phis)
+    if d3:
+        if clear:
+            b3.clear()
+        b3.add_points([x, y, z], method)
+        b3.show()
+    else:
+        if clear:
+            b.clear()
+        b3.add_points([x, y, z], method)
+        b.show()
+    if return_points:
+        return x, y, z
+
+
+def refresh_N(eta):
+    return Qobj(np.array([np.sin(eta) / np.sqrt(2), 0, np.cos(eta), 0, np.sin(eta) / np.sqrt(2)]))
 
 
 eta = np.pi/3
