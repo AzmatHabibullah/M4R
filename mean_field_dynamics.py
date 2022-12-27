010851c7f5187bf5432e1f69e7a85ef31a435c6d
Azmat Habibullah <ah5117@ic.ac.uk>
2022-12-27 14:34:13 +0000

    reformatting

--- mean_field_dynamics.py
+++ mean_field_dynamics.py
@@ -9,14 +9,15 @@ import matplotlib.pyplot as plt
 def skew(v):
     return np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
 
+
 def rotation_from_e1(vec, alpha, N=50):
     e1 = np.array([1, 0, 0])
     v = np.cross(e1, vec)
     c = np.dot(e1, vec)
-    R = np.eye(3) + skew(v) + skew(v).dot(skew(v))/(1 + c)
-    xp = [np.cos(alpha) for t in np.linspace(0, 2*np.pi, N)]
-    yp = [np.sin(alpha)*np.cos(t) for t in np.linspace(0, 2*np.pi, N)]
-    zp = [np.sin(alpha)*np.sin(t) for t in np.linspace(0, 2*np.pi, N)]
+    R = np.eye(3) + skew(v) + skew(v).dot(skew(v)) / (1 + c)
+    xp = [np.cos(alpha) for t in np.linspace(0, 2 * np.pi, N)]
+    yp = [np.sin(alpha) * np.cos(t) for t in np.linspace(0, 2 * np.pi, N)]
+    zp = [np.sin(alpha) * np.sin(t) for t in np.linspace(0, 2 * np.pi, N)]
     return R.dot([xp, yp, zp])
 
 
@@ -29,19 +30,18 @@ def rotation_from_e1(vec, alpha, N=50):
     b3.show()
 """
 
-def circular_sphere(a, b, g, precision = 100, cartesian=True):
-    t = np.linspace(0, 2*np.pi, precision)
-    cost = np.cos(t)
-    sint = np.sin(t)
-    x = np.sin(a)*np.cos(b)*np.cos(g)*np.cos(t) + \
-        np.sin(a)*np.sin(g)*np.sin(t) - np.cos(a)*np.sin(b)*np.cos(g)
+
+def circular_sphere(a, b, g, precision=100, cartesian=True):
+    t = np.linspace(0, 2 * np.pi, precision)
+    x = np.sin(a) * np.cos(b) * np.cos(g) * np.cos(t) + \
+        np.sin(a) * np.sin(g) * np.sin(t) - np.cos(a) * np.sin(b) * np.cos(g)
     y = -np.sin(a) * np.cos(b) * np.sin(g) * np.cos(t) + \
-        np.sin(a) * np.cos(g) * np.sin(t) - np.cos(a) * np.sin(b) * np.sin(g)
-    z = np.sin(a)*np.sin(b)*np.cos(t) + np.cos(a)*np.cos(b)
+        np.sin(a) * np.cos(g) * np.sin(t) + np.cos(a) * np.sin(b) * np.sin(g)
+    z = np.sin(a) * np.sin(b) * np.cos(t) + np.cos(a) * np.cos(b)
     if cartesian:
         return np.array([x, y, z])
-    theta = np.arccos(z/(np.sqrt(x**2 + y**2 + z**2)))
-    phi = np.arctan(y/x)
+    theta = np.arccos(z / (np.sqrt(x ** 2 + y ** 2 + z ** 2)))
+    phi = np.arctan(y / x)
     return np.array([theta, phi])
 
 
@@ -53,20 +53,22 @@ def plot_evolution(spinors, d3=False, method='s', clear=True, return_psi=False, 
     :param method:
     """
     points, dim = spinors.shape
-    F = (dim-1)/2
+    F = (dim - 1) / 2
     cmap = cm.get_cmap('viridis', points)
     if d3:
         bloch = b3
         bloch.point_size = 0.06
     else:
         bloch = b
-        bloch.point_size = 45
+        bloch.point_size = [45]  # for some reason this takes a list
     bloch.point_color = [matplotlib.colors.rgb2hex(cmap(i)) for i in range(cmap.N)]
     bloch.point_marker = ['o']
-    theta_evolution = np.zeros([points, int(2*F)])
-    phi_evolution = np.zeros([points, int(2*F)])
+    theta_evolution = np.zeros([points, int(2 * F)])
+    phi_evolution = np.zeros([points, int(2 * F)])
     for i, spinor in enumerate(spinors):
         _, thetas, phis = projection_onto_spin_half(Qobj(spinor))
+        print(thetas)
+        print(phis)
         theta_evolution[i] = thetas
         phi_evolution[i] = phis
 
@@ -74,11 +76,11 @@ def plot_evolution(spinors, d3=False, method='s', clear=True, return_psi=False, 
                     show=False, method=method)
     bloch.show()
     if plot_p1:
-        #print(theta_evolution)
-        #print(phi_evolution)
-        for i in range(int(2*F)):
-            plt.plot(np.linspace(1, points, points), theta_evolution[:, i], label="$\\theta_%d$" % (i+1))
-            plt.plot(np.linspace(1, points, points), phi_evolution[:, i], label="$\\phi_%d$" % (i+1))
+        # print(theta_evolution)
+        # print(phi_evolution)
+        for i in range(int(2 * F)):
+            plt.plot(np.linspace(1, points, points), theta_evolution[:, i], label="$\\theta_%d$" % (i + 1))
+            plt.plot(np.linspace(1, points, points), phi_evolution[:, i], label="$\\phi_%d$" % (i + 1))
         # todo change to show t on the x axis
         # todo fix theta and phi tracking
         plt.legend()
@@ -94,22 +96,25 @@ def plot_evolution(spinors, d3=False, method='s', clear=True, return_psi=False, 
 def solve_system(initial_value, t0=0, t1=1, precision=100, draw_m=False, d3=False, func=None):
     if func is None:
         func = c_nought_zero
-    alpha = np.pi/3
-    f = 1+np.sin(alpha)/(np.sqrt(2)*np.cos(alpha))
+    alpha = np.pi / 3
+    f = 1 + np.sin(alpha) / (np.sqrt(2) * np.cos(alpha))
     psi0 = Qobj(initial_value).unit()
-    F = (initial_value.size-1)/2 # todo fix
+    F = (initial_value.size - 1) / 2  # todo fix
     m = Qobj(np.array([expect(jmat(F, "x"), psi0), expect(jmat(F, "y"), psi0),
                        expect(jmat(F, "z"), psi0)]))
     b.vector_color = ["b", "b", "c", "c", "y", "y"]
 
     if draw_m and np.linalg.norm(m) != 0.0:
         draw_spinor_projection(m, d3=d3, clear=False, show=False)
+        print("m\n ", m)
+        print("magnitude of m is", np.linalg.norm(m))
 
     def zfunc(psi, t):
         return func(psi, m)
+
     t = np.linspace(t0, t1, precision)
 
-    psi, infodict = odeintz(zfunc, initial_value/np.linalg.norm(initial_value), t, full_output=True)
+    psi, infodict = odeintz(zfunc, initial_value / np.linalg.norm(initial_value), t, full_output=True)
     return psi, infodict
 
 
@@ -146,18 +151,19 @@ def odeintz(func, z0, t, **kwargs):
         return z
 
 
-def plot(ic, precision=50, t1=1, draw_m=True, return_psi=False, d3=False, func=None, plot_p1=False):
+def plot(ic, precision=50, t1=1, draw_m=True, return_psi=False, d3=False, func=None, plot_p1=False, clear=True):
     if func is None:
         func = c_nought_zero
     psi, infodict = solve_system(ic, t1=t1, precision=precision, draw_m=draw_m, func=func)
     draw_spinor_projection(Qobj(ic), d3=d3, clear=False, show=False)
     draw_spinor_projection(Qobj(psi[-1]), d3=d3, clear=False, show=False)
-    plot_evolution(psi, clear=True, plot_p1=plot_p1, d3=d3)
+    plot_evolution(psi, clear=clear, plot_p1=plot_p1, d3=d3)
     if return_psi:
         return psi
 
+
 def plot_zero_m(precision=100, tol=10e-5, F=1):
-    vec = np.random.normal(size=(precision,3))
+    vec = np.random.normal(size=(precision, 3))
     vec /= np.linalg.norm(vec, axis=1)[:, np.newaxis]
     xp = vec[:, 0]
     yp = vec[:, 1]
@@ -180,7 +186,27 @@ def mean_field_RHS(F=1, c0=1, c2=1):
         p = Qobj(psi)
         m = Qobj(np.array([expect(jmat(F, "x"), p), expect(jmat(F, "y"), p),
                            expect(jmat(F, "z"), p)]))
-        return c2*(m[0, 0]*jmat(F, "x") + m[1, 0]*jmat(F, "y") + m[2, 0]*jmat(F, "z"))*psi*1/1.0j + c0*1/1.0j*psi*np.abs(psi**2)
+        return c2 * (m[0, 0] * jmat(F, "x") + m[1, 0] * jmat(F, "y") + m[2, 0] * jmat(F,
+                                                                                      "z")) * psi * 1 / 1.0j + c0 * 1 / 1.0j * psi  # *np.abs(psi**2)
+
     return to_return
 
 
+def spin_2_RHS(F=2, c0=1, c1=1, c2=1):
+    def to_return(psi, m):
+        p = Qobj(psi)
+        A = (2 * psi[0] * psi[-1] - 2 * psi[1] * psi[-2] + psi[3] ** 2) / np.sqrt(5)
+        rhs = c0 * psi
+        fz = expect(jmat(F, "z"), p)
+        fplus = expect(jmat(F, "+"), p)
+        fminus = np.conjugate(fplus)
+        rhs[2] += np.sqrt(6) / 2 * c1 * (fplus * psi[1] + fminus * psi[-2]) \
+                  + c2 / np.sqrt(5) * A * np.conjugate(psi[2])
+        rhs[0] += 2 * c1 * fz * psi[0] + c1 * fminus * psi[1] + c2/np.sqrt(5)*A*np.conjugate(psi[-1])
+        rhs[-1] += -2*c1 * fz * psi[-1] + c1 * fplus*psi[-2] + c2/np.sqrt(5)*A*np.conjugate(psi[0])
+        rhs[1] += c1*fz*psi[1] + c1*(np.sqrt(6)/2 * fminus * psi[2] + fplus * psi[0]) - \
+                  c2/np.sqrt(5)*A*np.conjugate(psi[-2])
+        rhs[-2] += -c1*fz*psi[-2] + c1*(np.sqrt(6)/2*fplus*psi[2] + fminus * psi[-1]) - \
+                   c2/np.sqrt(5)*A*np.conjugate(psi[1])
+        return rhs
+    return to_return
