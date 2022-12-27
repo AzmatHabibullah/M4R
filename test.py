acd376e0ccefd564ecc17f46f79ddbbff594c069
Azmat Habibullah <ah5117@ic.ac.uk>
2022-12-27 14:31:38 +0000

    update tests

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from mean_field_dynamics import solve_system, mean_field_RHS
-from representation import construct_polar_states, polar_to_point, projection_onto_spin_half
+from representation import polar_to_point, projection_onto_spin_half
 import pytest
 import numpy as np
 from qutip import *
@@ -8,13 +8,14 @@ spin_1_tests = [(1, 100, np.array([1, 2, 3])),(1, 34, np.array([np.exp(1.0j*np.p
               (1, 68, np.array([1, np.exp(np.pi/3*1j), np.exp(1.0j*np.pi/4)])),
               (1, 68, np.array([1, 9, 3j])), (1, 22, np.array([1, 0, 3j])), (2, 50, np.array([1.0j, 3.5j - 5, 49j]))]
 
-spin_2_tests = [(2, 40, np.array([1, 1, 1, 1, 1])),
-              (2, 34, np.array([-6j + 3, 2, 9j, 18, 5])),
+spin_2_tests =[
+              (1, 34, np.array([-6j + 3, 2, 9j, 18, 5])),
               (2, 50, np.array([1.0j, 3j, 5, 3.5j - 5, 49j]))]
 
 failed_tests = [(1, 50, np.array([0, 4, 3j]))]
 # n1+n2 conserved test fail only. is this because m is conserved in spin 1?
 # what's conserved for spin 2?
+
 
 @pytest.mark.parametrize('c2, precision, psi0', spin_1_tests)
 def test_m_conserved_for_c0_zero(c2, precision, psi0):
@@ -28,13 +29,13 @@ def test_m_conserved_for_c0_zero(c2, precision, psi0):
     thetas = np.zeros([precision, int(2*F)])
     phis = np.zeros([precision, int(2*F)])
     _, thetas[0], phis[0] = projection_onto_spin_half(Qobj(psi0))
-    normals0 = np.zeros([int(2*F), 3])
-    for i in range(int(2*F)):
-        x0, y0, z0 = polar_to_point(thetas[0, i], phis[0, i])
-        normals0[i, :] = np.array([x0, y0, z0])
-    #x0, y0, z0 = polar_to_point(thetas, phis)
-    #normals0 = np.array([x0, y0, z0])
+
+    x0, y0, z0 = polar_to_point(thetas[0], phis[0])
+    normals0 = np.array([x0, y0, z0]).T
+
+    normals0 = np.sort(normals0, axis=0)
     n0 = np.array([np.sum(normals0[2*i:2*i+2], axis=0) for i in range(int(F))])
+    alpha0 = np.dot(normals0[0], normals0[1])
     for i, psi in enumerate(psi_evolution):
         psi = Qobj(psi)
         m[i, 0] = expect(jmat(F, "x"), psi)
@@ -44,22 +45,18 @@ def test_m_conserved_for_c0_zero(c2, precision, psi0):
             assert np.linalg.norm(m[i, :] - m[0, :]) < 10e-5                  # check m constant
             _, thetas[i], phis[i] = projection_onto_spin_half(Qobj(psi))
             x, y, z = polar_to_point(thetas[i], phis[i])
-            normals = np.array([x, y, z])
-            #for j in range(int(2*F)):
-            #    assert np.linalg.norm(np.linalg.norm(normals[j]) - 1) < 10e-5     # check ni normalised
-            n = [np.sum(normals[:, 2*i:2*i+2], axis=1) for i in range(int(F))]
+            normals = np.array([x, y, z]).T
+            normals = np.sort(normals, axis=0)
+            n = np.array([np.sum(normals[2*i:2*i+2], axis=0) for i in range(int(F))])
             n1 = np.array([x[0], y[0], z[0]])
             n2 = np.array([x[1], y[1], z[1]])
-            assert np.linalg.norm(n - n0) < 10e-5
-            alpha = np.dot(normals[0], normals[1])
-            alpha0 = np.dot(n1, n2)
-            #assert np.linalg.norm(alpha - alpha0) < 10e-5
+            assert np.linalg.norm(n - n0) < 10e-5                             # check n1 + n2 conserved
+            alpha = np.dot(n1, n2)                                           # check angle between conserved
+            assert np.linalg.norm(alpha - alpha0) < 10e-5
 
 
-    # check if n1, n2 and n1.n2 conserved. do using polar representation with generated psi.
     # n1 + n2 conserved for F = 1 (check with more cases)
-    # calculate representation, convert to cartesian, iterate through and check conserved.
-
+    # check what is conserved for F = 2. why is n1 + n2 conserved for F=1? what's significant about this?
 
 @pytest.mark.parametrize('c0, precision, psi0', spin_2_tests)
 def test_m3_conserved_for_c2_zero(c0, precision, psi0):
@@ -78,6 +75,7 @@ def test_m3_conserved_for_c2_zero(c0, precision, psi0):
 
 # test n1 + n2 conserved, for c2=0
 # test n1.n2 conserved, for c2=0
+
 
 @pytest.mark.parametrize('F', [(1)])
 def test_antipodal_conserved(F):
